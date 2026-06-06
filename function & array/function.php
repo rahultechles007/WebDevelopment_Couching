1a051fe359c84c0c1aeb6ae331901cc1a404cf79
rahultechles007 <binodkumarpradhan145@gmail.com>
2026-06-06 20:37:15 +0530

    10th commit

---
+++ function & array/function.php
@@ -0,0 +1,7 @@
+<!-- As we know there are differnt types of  function 
+1. Built in function 
+2. user define function - user can able to define any function from this  -->
+
+
+<!-- simple example of funtion  -->
+ 
